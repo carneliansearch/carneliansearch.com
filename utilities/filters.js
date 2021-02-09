f6058a1b26fb8c2dd035919d265db0e17091a29b
James Doc <hi@jamesdoc.com>
2021-02-09 09:07:49 +0000

    Pull strapline from metadata

--- utilities/filters.js
+++ utilities/filters.js
@@ -2,5 +2,10 @@
 module.exports = {
   findBySlug: (collection, slug) => {
     return collection.find((item) => item.fileSlug === slug);
+  },
+
+  protectRunts: (string) => {
+    const index = string.lastIndexOf(' ');
+    return string.substr(0, index) + '&nbsp;' + string.substr(index + 1);
   }
 }
